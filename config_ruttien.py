57e78a82484e06016988a16ded5181e1c2d3cf48
TDC <phuoctrung.le@southedge.vn>
2023-09-06 09:09:13 +0700

    update file

---
+++ config_ruttien.py
@@ -0,0 +1,9 @@
+quan_config = {
+    #     username, merchanID,password, qr_code_img
+    "anvatmrx": ["anvatmrx", "Anhoianhoi13579", "193936",""],
+    "banh_xeo_qngai": ["0905060588", "12345678", "1129949",""],
+    "tap_hoa_phuong_pham": ["0935320845", "Trung@30", "1135981",""],
+    "my_quang_que_huong": ["myquangquehuong", "Trung@30", "1174728",""]
+}
+
+
